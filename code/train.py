de2b0445d17e49e192f700205f560bcba6553f30
sublatelog <blueganz@gmail.com>
2022-06-24 08:30:22 +0900

    Update train.py

--- code/train.py
+++ code/train.py
@@ -93,7 +93,7 @@ def train(model, train_loader, val_loader, epochs):
 
     num_train_optimization_steps = int(args.epochs * len(train_loader) / args.accumulation_steps)
     optimizer = AdamW(optimizer_grouped_parameters, 
-                      lr=9e-5, # 3e-5,
+                      lr=9e-4, # 9e-5, # 3e-5,
                       correct_bias=False
                      )  # To reproduce BertAdam specific behavior set correct_bias=False
     scheduler = get_linear_schedule_with_warmup(optimizer, num_warmup_steps=0.05 * num_train_optimization_steps,
